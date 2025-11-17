e64ad8152b310570728011901efa6a119875be4a
Your Name <2024302111432@whu.edu.cn>
2025-11-17 16:29:44 +0800

    初始框架

---
+++ MyEngine/My Engine/My Engine Core/src/graphics/Shader.h
@@ -0,0 +1,35 @@
+#pragma once
+#include"../utils/FileUtils.h"
+#include<GL/glew.h>
+#include<glm/glm.hpp>
+#include<glm/gtc/type_ptr.hpp>
+#include<iostream>
+#include<vector>
+namespace myarcane {
+	namespace graphics {
+		class Shader {
+		private:
+			GLuint m_ShaderID;
+			const char* m_VertPath;
+			const char* m_FragPath;
+		public:
+			Shader(const char* vertPath, const char* fragPath);
+			~Shader();
+
+			void setUniform1f(const GLchar* name, float value);
+			void setUniform1i(const GLchar* name, int value);
+			void setUniform2f(const GLchar* name, const glm::vec2& vector);
+			void setUniform3f(const GLchar* name, const glm::vec3& vector);
+			void setUniform4f(const GLchar* name, const glm::vec4& vector);
+			void setUniformMat4(const GLchar* name, const glm::mat4& matrix);
+
+			void enable() const;
+			void disable() const;
+
+			inline GLuint getShaderID() { return m_ShaderID; }
+		private:
+			GLint getUniformLocation(const GLchar* name);
+			GLuint load();
+		};
+	}
+}
---
+++ MyEngine/My Engine/My Engine Core/src/graphics/camera/FPSCamera.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include<GL/glew.h>
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+
+namespace myarcane {
+	namespace graphics {
+		enum Camera_Movement {
+			FORWARD,
+			BACKWARD,
+			LEFT,
+			RIGHT
+		};
+
+		const GLfloat YAW = -90.0f;//水平偏航角(水平)
+		const GLfloat PITCH = 0.0f;//俯仰角（上下）
+		const GLfloat SPEED = 3.0f;//移动速度
+		const GLfloat SENSITIVITY = 0.25f;//鼠标灵敏度
+		const GLfloat FOV = 45.0f;//视野范围
+
+		class FPSCamera {
+		public:
+			//相机位置朝向
+			glm::vec3 m_Position;
+			glm::vec3 m_Front;
+			glm::vec3 m_Up;
+			glm::vec3 m_Right;
+			glm::vec3 m_WorldUp;
+			//相机参数角度
+			GLfloat m_Yaw;
+			GLfloat m_Pitch;
+			//Camera options
+			GLfloat m_MovementSpeed;
+			GLfloat m_MouseSensitivity;
+			GLfloat m_Fov;
+		public:
+			//向量构造函数
+			FPSCamera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), GLfloat yaw = YAW, GLfloat pitch = PITCH);
+			//坐标构造函数
+			FPSCamera(GLfloat xPos, GLfloat yPos, GLfloat zPos, GLfloat xUp, GLfloat yUp, GLfloat zUp, GLfloat yaw, GLfloat pitch);
+
+			glm::mat4 getViewMatrix();
+			void processKeyboard(Camera_Movement direction, GLfloat deltaTime);
+			void processMouseMovement(GLfloat xOffset, GLfloat yOffset, GLboolean constrainPitch=true);
+			void processMouseScroll(GLfloat yOffset);
+
+			//inline GLfloat getYaw() { return m_Yaw; };
+			//inline GLfloat getPitch() { return m_Pitch; };
+			//inline GLfloat getFov() { return m_Fov; };
+			//inline GLfloat getMovementSpeed() { return m_MovementSpeed; };
+			//inline GLfloat getMouseSensitivity() { return m_MouseSensitivity; };
+		private:
+			void updateCameraVectors();
+		};
+	}
+}
