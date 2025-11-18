d6b1a28ea65ddff2d8aa1be7f41e2a3aa98a8016
zhangchen667 <2271875988@qq.com>
2025-11-18 15:14:49 +0800

    基础光照与物体材质

--- MyEngine/My Engine/My Engine Core/src/main.cpp
+++ MyEngine/My Engine/My Engine Core/src/main.cpp
@@ -143,12 +143,23 @@ int main() {
 
 		// 光源变量的设置
 		shader.enable();
-		shader.setUniform3f("objectColour", glm::vec3(1.0f, 0.5f, 0.31f));
-		shader.setUniform3f("lightColour", glm::vec3(1.0f, 1.0f, 1.0f));
-		shader.setUniform3f("lightPosition", glm::vec3(lightPos.x, lightPos.y, lightPos.z));
+		glm::vec3 lightColour;
+		lightColour.x = sin(count.elapsed() * 2.0f);
+		lightColour.y = sin(count.elapsed() * 0.7f);
+		lightColour.z = sin(count.elapsed() * 1.3f);
+		glm::vec3 diffuseColour = lightColour * glm::vec3(0.5f);
+		glm::vec3 ambientColour = diffuseColour * glm::vec3(0.2f);
 		glm::vec3 cameraPosition = camera.getPosition();
 		shader.setUniform3f("viewPos", glm::vec3(cameraPosition.x, cameraPosition.y, cameraPosition.z));
 
+		shader.setUniform3f("material.ambient", glm::vec3(0.2f, 0.2f, 0.2f));
+		shader.setUniform3f("material.diffuse", glm::vec3(0.5f, 0.5f, 0.5f));
+		shader.setUniform3f("material.specular", glm::vec3(1.0f, 1.0f, 1.0f));
+		shader.setUniform1f("material.shininess", 32.0f);
+		shader.setUniform3f("light.position", glm::vec3(lightPos.x, lightPos.y, lightPos.z));
+		shader.setUniform3f("light.ambient", glm::vec3(ambientColour.x, ambientColour.y, ambientColour.z));
+		shader.setUniform3f("light.diffuse", glm::vec3(diffuseColour.x, diffuseColour.y, diffuseColour.z));
+		shader.setUniform3f("light.specular", glm::vec3(1.0f, 1.0f, 1.0f));
 		glm::mat4 model(1);
 		//model = glm::rotate(model, (GLfloat)count.elapsed(), glm::vec3(1.0f, 0.3f, 0.6f));//旋转矩阵（随时间）
 
