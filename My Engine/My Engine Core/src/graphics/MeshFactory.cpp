f82a4f3362f5436c333b4da30e9f9a5040f56cf5
zhangchen667 <2271875988@qq.com>
2025-12-09 14:52:22 +0800

    添加帧缓冲着色器以及帧缓冲网格函数

--- My Engine/My Engine Core/src/graphics/MeshFactory.cpp
+++ My Engine/My Engine Core/src/graphics/MeshFactory.cpp
@@ -30,5 +30,24 @@ namespace myarcane {
 			textures.push_back(texture);
 			return new Mesh(vertices, indices, textures);
 		}
+		Mesh* MeshFactory::CreateScreenQuad(int colourBufferId) {
+			std::vector<Vertex>vertices;
+			std::vector<unsigned int>indices;
+			std::vector<Texture>textures;
+			//定义四个顶点
+			vertices.push_back(Vertex(glm::vec3(-1, 1, 0), glm::vec3(0, 0, 0), glm::vec2(0, 1)));
+			vertices.push_back(Vertex(glm::vec3(1, 1, 0), glm::vec3(0, 0, 0), glm::vec2(1, 1)));
+			vertices.push_back(Vertex(glm::vec3(-1, -1, 0), glm::vec3(0, 0, 0), glm::vec2(0, 0)));
+			vertices.push_back(Vertex(glm::vec3(1, -1, 0), glm::vec3(0, 0, 0), glm::vec2(1, 0)));
+			//定义indices
+			indices.push_back(1); indices.push_back(0); indices.push_back(2);
+			indices.push_back(3); indices.push_back(1); indices.push_back(2);
+			//加载纹理
+			Texture texture;
+			texture.id = colourBufferId;
+			texture.type = "texture_diffuse";
+			textures.push_back(texture);
+			return new Mesh(vertices, indices, textures);
+		}
 	}
 }
